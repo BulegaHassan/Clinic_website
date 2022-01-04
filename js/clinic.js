eae196a72c5ba3c450011ab8a42e23aa58eb02a6
BulegaHassan <hassanbulega@gmail.com>
2022-01-04 09:07:32 +0300

    replacing  icons and improving responsive design

--- js/clinic.js
+++ js/clinic.js
@@ -1,16 +1,5 @@
 "use strict";
 $(document).ready(() => {
-    //For Animations
-    $("#animate h2").animate(
-		{ fontSize: "200%", opacity: 1, left: "+=175" }, 
-		2000,
-		 () => {
-			$("#animate h2").next().fadeIn(2000); 
-		}
-       
-	)
-    .animate( 
-        { fontSize: "175%", left: "-=275" }, 2000 );
      //For imagerollovers
     
     $("#image_rollovers img").each( (index, img) => {
